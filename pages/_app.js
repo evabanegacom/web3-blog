78b85100a88f121f485212c50180922746ed5503
Precious Udegbue <Precious.Udegbue@sterling.ng>
2022-07-08 19:02:38 +0100

    implemented UI component

--- pages/_app.js
+++ pages/_app.js
@@ -41,6 +41,65 @@ function MyApp({ Component, pageProps }) {
     }
   }
 
+  return (
+    <div>
+      <nav className={nav}>
+        <div className={header}>
+          <Link href="/">
+            <a>
+              <img
+                src='/logo.svg'
+                alt="React Logo"
+                style={{ width: '50px' }}
+              />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <div className={titleContainer}>
+                <h2 className={title}>Full Stack</h2>
+                <p className={description}>WEB3</p>
+              </div>
+            </a>
+          </Link>
+          {
+            !account && (
+              <div className={buttonContainer}>
+                <button className={buttonStyle} onClick={connect}>Connect</button>
+              </div>
+            )
+          }
+          {
+            account && <p className={accountInfo}>{account}</p>
+          }
+        </div>
+        <div className={linkContainer}>
+          <Link href="/" >
+            <a className={link}>
+              Home
+            </a>
+          </Link>
+          {
+            /* if the signed in user is the contract owner, we */
+            /* show the nav link to create a new post */
+            (account === ownerAddress) && (
+              <Link href="/create-post">
+                <a className={link}>
+                  Create Post
+                </a>
+              </Link>
+            )
+          }
+        </div>
+      </nav>
+      <div className={container}>
+        <AccountContext.Provider value={account}>
+          <Component {...pageProps} connect={connect} />
+        </AccountContext.Provider>
+      </div>
+    </div>
+  )
+
 }
 
 export default MyApp
