af34371e217db6c0c697df335bf7a0029ecd71ec
Precious Udegbue <Precious.Udegbue@sterling.ng>
2022-07-08 18:27:26 +0100

    setup context

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import '../styles/globals.css'
+import { useState } from 'react'
+import Link from 'next/link'
+import { css } from '@emotion/css'
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { AccountContext } from '../context.js'
+import { ownerAddress } from '../config'
+import 'easymde/dist/easymde.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
