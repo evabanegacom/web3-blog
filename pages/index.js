75529b92433c2010d16a353579c42c3cbe7b6e3d
Precious Udegbue <Precious.Udegbue@sterling.ng>
2022-07-09 15:01:08 +0100

    created function to communicate with network providers

--- pages/index.js
+++ pages/index.js
@@ -48,8 +48,45 @@ export default function Home(props) {
           ))
         }
       </div>
+      <div className={container}>
+        {
+          (account === ownerAddress) && posts && !posts.length && (
+            /* if the signed in user is the account owner, render a button */
+            /* to create the first post */
+            <button className={buttonStyle} onClick={navigate}>
+              Create your first post
+              <img
+                src='/right-arrow.svg'
+                alt='Right arrow'
+                className={arrow}
+              />
+            </button>
+          )
+        }
+      </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  /* here we check to see the current environment variable */
+  /* and render a provider based on the environment we're in */
+  let provider
+  if (process.env.ENVIRONMENT === 'local') {
+    provider = new ethers.providers.JsonRpcProvider()
+  } else if (process.env.ENVIRONMENT === 'testnet') {
+    provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.matic.today')
+  } else {
+    provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/')
+  }
+
+  const contract = new ethers.Contract(contractAddress, Blog.abi, provider)
+  const data = await contract.fetchPosts()
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(data))
+    }
+  }
 }
 
 const arrowContainer = css`
