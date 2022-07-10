fc2d0451eba05fceeeba4eeb80e89d6d71a5b845
Precious Udegbue <udegbue69@gmail.com>
2022-07-10 11:31:14 +0100

    created UI for adding posts

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
-import { css } from '@emotion/css';
-import { useContext } from 'react';
-import { useRouter } from 'next/router';
-import { ethers } from 'ethers';
-import Link from 'next/link';
-import { AccountContext } from '../context';
+import { css } from '@emotion/css'
+import { useContext } from 'react'
+import { useRouter } from 'next/router'
+import { ethers } from 'ethers'
+import Link from 'next/link'
+import { AccountContext } from '../context'
 
 /* import contract address and contract owner address */
 import {
   contractAddress, ownerAddress
-} from '../config';
+} from '../config'
 
 /* import Application Binary Interface (ABI) */
-import Blog from '../artifacts/contracts/Blog.sol/Blog.json';
+import Blog from '../artifacts/contracts/Blog.sol/Blog.json'
 
 export default function Home(props) {
-
   /* posts are fetched server side and passed in as props */
   /* see getServerSideProps */
   const { posts } = props
@@ -80,7 +79,7 @@ export async function getServerSideProps() {
     provider = new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/')
   }
 
-  const contract = new ethers.Contract(contractAddress, Blog.abi, provider)
+  const contract = new ethers.Contract(contractAddress, Blog?.abi, provider)
   const data = await contract.fetchPosts()
   return {
     props: {
